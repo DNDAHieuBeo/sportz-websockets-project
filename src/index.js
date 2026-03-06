e0a5b83a65d340900f25387d137041e86cdc4f1a
Hieu Le <hieu.le@enlabsoftware.com>
2026-03-06 16:43:36 +0700

    implement arcjet security

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import http from "http";
 import { matchRouter } from "./routes/matches.js";
 import { attachWebSocketServer } from "./ws/server.js";
+import { securityMiddleware } from "./arcjet.js";
 
 const PORT = Number(process.env.PORT || 8000);
 const HOST = process.env.HOST || "0.0.0.0";
@@ -9,24 +10,27 @@ const HOST = process.env.HOST || "0.0.0.0";
 const app = express();
 const server = http.createServer(app);
 
-// JSON middleware
 app.use(express.json());
 
-// Root GET route
 app.get("/", (req, res) => {
-  res.send("Welcome to the Sportz API! 🏆");
+  res.send("Hello from Express server!");
 });
+
+app.use(securityMiddleware());
 
 app.use("/matches", matchRouter);
 
-const { broadcastMatchUpdate } = attachWebSocketServer(server);
-app.locals.broadcastMatchUpdate = broadcastMatchUpdate;
+const { broadcastMatchCreated, broadcastCommentary } =
+  attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+app.locals.broadcastCommentary = broadcastCommentary;
 
-// Start server
 server.listen(PORT, HOST, () => {
   const baseUrl =
     HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
-  console.log(`Server is running at ${baseUrl}`);
-  console.log(`WebSocket endpoint available at ws://localhost:${PORT}/ws`);
-});
 
+  console.log(`Server is running on ${baseUrl}`);
+  console.log(
+    `WebSocket Server is running on ${baseUrl.replace("http", "ws")}/ws`,
+  );
+});
